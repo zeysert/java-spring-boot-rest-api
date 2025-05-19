caff6075f63253178c72f5d46c84c38b953172a5
zeynep sert <zeysert@outlook.com>
2025-05-19 16:56:42 +0300

    Update FirstApiController.java

--- restful-web-services/src/main/java/com/rest/example/medium/restful_web_services/firstapi/FirstApiController.java
+++ restful-web-services/src/main/java/com/rest/example/medium/restful_web_services/firstapi/FirstApiController.java
@@ -6,32 +6,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FirstApiController {
 
+    @GetMapping(path = "/hello-rest")
+    public String helloREST() {
+        return "Hello, it's my first REST API!";
+    }
 
-	@GetMapping(path = "/hello-rest")
-	public String helloREST() {
-		return "Hello it's my first rest api"; 
-	}
-	
-	@GetMapping(path = "/hello-rest-two")
-	public String helloRESTTwo() {
-		return "Hello it's my second rest api"; 
-	}
-	
-	@GetMapping(path = "/hello-rest-WithClass")
-	public WithClass helloRESTWithClass() {
-		return new  WithClass("Hello it's my  rest api that created with class"); 
-	}
-	
-	@GetMapping(path = "/hello-rest-WithMethod")
-	public String helloRESTWithMethod() {
-		return WithMethod("Hello it's my  rest api that created with method"); 
-	}
+    @GetMapping(path = "/hello-rest-two")
+    public String helloRESTTwo() {
+        return "Hello, it's my second REST API!";
+    }
 
-	private String WithMethod(String string) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	
+    @GetMapping(path = "/hello-rest-with-class")
+    public WithClass helloRESTWithClass() {
+        return new WithClass("Hello from a class!");
+    }
 
+    @GetMapping(path = "/hello-rest-with-method")
+    public String helloRESTWithMethod() {
+        return withMethod("Hello from a method!");
+    }
+
+    private String withMethod(String message) {
+        return message;
+    }
 }
